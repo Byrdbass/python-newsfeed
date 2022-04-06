3bfcc0f559f1f886edf62a8d4435eea9cebafdd5
ByrdBassPC <byrdbass@gmail.com>
2022-04-06 14:53:42 -0500

    made basic structure of a pip package with Flask, and created first working code of create_app with __init__.py file

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+# we import the Flask function and use the 'def' keyword to define what create_app() does
+from flask import Flask 
+# when Flask runs the app package it will be looking for create_app() function
+def create_app(test_config=None):
+    #set up app config
+    app = Flask(__name__, static_url_path='/')
+    app.url_map.strict_slashes = False
+    app.config.from_mapping(
+        SECRET_KEY='byrd_is_the_word'
+    )
+
+    return app
