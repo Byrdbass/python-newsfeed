3c23a81e0bf9ef9a38efd0080e6689f5db18871f
ByrdBassPC <byrdbass@gmail.com>
2022-04-08 18:52:20 -0500

    tested validators of email and password, and created post model using 'relationship' with ForeignKey

--- seeds.py
+++ seeds.py
@@ -1,4 +1,4 @@
-from app.models import User
+from app.models import User, Post
 from app.db import Session, Base, engine
 
 #drop and rebuild tables
@@ -13,7 +13,8 @@ db.add_all([
     User(username='jimmyJon', email='jimmyJon@sandwhich.com', password='factoryFood123'),
     User(username='FrankyGuy', email='frank@hotdog.com', password='kosherdills321'),
     User(username='MarthaDempsey', email='imanidiot@dumass.com', password='iBribeClassmates'),
-    User(username='DempCSees', email='illusionsOfGrandjeur@bootcamp.com', password='myHusbandHatesMe411')
+    User(username='DempCSees', email='illusionsOfGrandjeur@bootcamp.com', password='myHusbandHatesMe411'),
+    User(username='notValidnametest', email='jimmyjohns@jimmy.com', password='allgf')
 ])
 
 db.commit()
