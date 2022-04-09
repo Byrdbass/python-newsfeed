d833cf50490c7808107778a8e7e0ee401b2cada9
ByrdBassPC <byrdbass@gmail.com>
2022-04-08 19:22:03 -0500

    seeded the Posts table by making sure the db.add_all and db.commit came after populating the User seeds

--- seeds.py
+++ seeds.py
@@ -14,7 +14,19 @@ db.add_all([
     User(username='FrankyGuy', email='frank@hotdog.com', password='kosherdills321'),
     User(username='MarthaDempsey', email='imanidiot@dumass.com', password='iBribeClassmates'),
     User(username='DempCSees', email='illusionsOfGrandjeur@bootcamp.com', password='myHusbandHatesMe411'),
-    User(username='notValidnametest', email='jimmyjohns@jimmy.com', password='allgf')
+    User(username='notValidnametest', email='jimmyjohns@jimmy.com', password='allgf'),
+])
+
+db.commit()
+
+# we need to run the posts after the users are created because the Post model requires valid user_id fields
+
+db.add_all([
+    Post(title='Algorithm sites', post_url='https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/', user_id=1),
+    Post(title='Tech Jobs!', post_url='https://app.otta.com/', user_id=1),
+    Post(title='more tech jobs!', post_url='https://angel.co/jobs', user_id=1),
+    Post(title='Algoexpert', post_url='https://www.algoexpert.io/product?r=ads', user_id=2),
+    Post(title='HackerRank', post_url='https://www.hackerrank.com/dashboard', user_id=3)
 ])
 
 db.commit()
