c0b76b5f9756db67acc3c4b92201942621c725cf
ByrdBassPC <byrdbass@gmail.com>
2022-04-10 17:36:35 -0500

    started Vote model

--- seeds.py
+++ seeds.py
@@ -1,4 +1,4 @@
-from app.models import User, Post
+from app.models import User, Post, Comment
 from app.db import Session, Base, engine
 
 #drop and rebuild tables
@@ -27,6 +27,17 @@ db.add_all([
     Post(title='more tech jobs!', post_url='https://angel.co/jobs', user_id=1),
     Post(title='Algoexpert', post_url='https://www.algoexpert.io/product?r=ads', user_id=2),
     Post(title='HackerRank', post_url='https://www.hackerrank.com/dashboard', user_id=3)
+])
+
+db.commit()
+
+# add the comments post here
+db.add_all([
+    Comment(comment_text='This is a Udemy course reccomended to me by Juan', user_id=1, post_id=1),
+    Comment(comment_text='A website reccomended by Morgan', user_id=1, post_id=2),
+    Comment(comment_text='Great for Start ups!', user_id=1, post_id=3),
+    Comment(comment_text='this was a highly reccomended site from Leah', user_id=2, post_id=4),
+    Comment(comment_text='this is a very common algo site', user_id=3, post_id=5)
 ])
 
 db.commit()
