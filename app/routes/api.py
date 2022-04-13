0373e788f5464de92e3e50515401a372faf59617
ByrdBassPC <byrdbass@gmail.com>
2022-04-13 10:36:07 -0500

    successfully posted new users via login route, and used jsonfiy to specify return message.  Had an error with db.add 'attribute error' which was fixed by adding parenthesis to the db = get_db function.  Now need to import sys module to see the jsonify-ed error and success messages

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request
+import json
+from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
 
@@ -7,16 +8,20 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 @bp.route('/users', methods=['POST'])
 def signup():
     data = request.get_json()
-    db = get_db
+    db = get_db()
     
+    try:
     # we use the dictionary data type here instead of an object (dot notation) as we are not attaching methods to this data response
-    newUser = User(
-        username = data['username'],
-        email = data['email'],
-        password = data['password']
-    )
+        newUser = User(
+            username = data['username'],
+            email = data['email'],
+            password = data['password']
+        )
 
-    db.add(newUser)
-    db.commit()
+        db.add(newUser)
+        db.commit()
 
-    return ''
+    except:
+        return jsonify(message = 'Signup failed, may be an existing account, email incorrect or password is not long enough'), 500
+
+    return jsonify(id = newUser.id)
